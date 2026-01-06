f3505afbf5c2f2578f671582edf5aaa24b657676
euphoria-laxis <euphoria.laxis@euphoria-laxis.com>
2026-01-06 17:49:25 +0100

    Update hashing package tests

--- hashing/hashing_test.go
+++ hashing/hashing_test.go
@@ -8,12 +8,11 @@ import (
 )
 
 var (
-	randomString, hashedString string
-	opts                       []hashing.OptFunc
+	opts []hashing.OptFunc
 )
 
-// TestOptions test Hasher Options passed by function creational pattern.
-func TestOptions(t *testing.T) {
+// testSetOptions set Hasher options values.
+func testSetOptions() {
 	opts = []hashing.OptFunc{
 		hashing.SetMemory(32 * 1024), // 32 bits
 		hashing.SetParallelism(4),    // 4 concurrents actions
@@ -23,52 +22,103 @@ func TestOptions(t *testing.T) {
 	}
 }
 
+// TestOptions test Hasher Options passed by function creational pattern.
+func TestOptions(_ *testing.T) {
+	testSetOptions()
+}
+
+// TestRandomBytes test RandomBytes function.
+func TestRandomBytes(t *testing.T) {
+	testSetOptions()
+	hasher := hashing.NewHasher(opts...)
+	seeds := []uint32{32, 64, 128, 256, 512, 1024, 2048}
+	for _, seed := range seeds {
+		rdm, err := hasher.RandomBytes(seed)
+		if err != nil {
+			t.Errorf("error generating random bytes: %v", err)
+		}
+		if len(rdm) != int(seed) {
+			t.Errorf("wrong random bytes length, expected 32, got %d", len(rdm))
+		}
+	}
+}
+
+// TestRandomString test RandomString function.
+func TestRandomString(t *testing.T) {
+	testSetOptions()
+	hasher := hashing.NewHasher(opts...)
+	seeds := []uint32{32, 64, 128, 256, 512, 1024, 2048}
+	for _, seed := range seeds {
+		rdm, err := hasher.RandomString(seed)
+		if err != nil {
+			t.Errorf("error generating random string: %v", err)
+		}
+		if len(rdm) != int(seed) {
+			t.Errorf("wrong random string length: got %d, want 32", len(rdm))
+		}
+	}
+}
+
 // TestHasher test if hasher correctly hashes strings.
 func TestHasher(t *testing.T) {
-	var err error
+	testSetOptions()
+	// Create a new Hasher instance.
 	hasher := hashing.NewHasher(opts...)
-	randomString, err = hasher.RandomString(32)
-	if err != nil {
-		log.Print(err)
-		t.Fail()
-	}
-	hashedString, err = hasher.HashString(randomString)
-	if err != nil {
-		log.Print(err)
-		t.Fail()
+	// Password shouldn't exceed 48 characters, so we'll avoid to go further.
+	seeds := []uint32{12, 16, 18, 20, 24, 28, 32}
+	for _, seed := range seeds {
+		// Generate a random string.
+		randomString, err := hasher.RandomString(seed)
+		if err != nil {
+			log.Print(err)
+			t.Fail()
+		}
+		// Hash the random string.
+		_, err = hasher.HashString(randomString)
+		if err != nil {
+			log.Print(err)
+			t.Fail()
+		}
 	}
 }
 
 // TestHashesCompare test if hashed strings can be compared.
 func TestHashesCompare(t *testing.T) {
+	testSetOptions()
 	// Create hasher.
-	hasher := hashing.NewHasher()
-	// Compared hash with original string.
-	match, err := hasher.CompareStringToHash(randomString, hashedString)
-	if err != nil {
-		log.Print(err)
-		t.Fail()
-	}
-	if !match {
-		log.Println("passwords comparison failed")
-		log.Println("passwords should match")
-		t.Fail()
-	}
-	//
-	hasher = hashing.NewHasher(opts...)
-	randomString, err = hasher.RandomString(32)
-	if err != nil {
-		log.Print(err)
-		t.Fail()
-	}
-	match, err = hasher.CompareStringToHash(randomString, hashedString)
-	if err != nil {
-		log.Print(err)
-		t.Fail()
-	}
-	if match {
-		log.Println("passwords comparison failed")
-		log.Println("passwords shouldn't match")
-		t.Fail()
+	hasher := hashing.NewHasher(opts...)
+	seeds := []uint32{12, 16, 18, 20, 24, 28, 32}
+	for _, seed := range seeds {
+		// Generate a random string
+		randomString, err := hasher.RandomString(seed)
+		if err != nil {
+			t.Fatalf("failed to generate random string: %s", err)
+		}
+		// Hash the random string.
+		hashedString, err := hasher.HashString(randomString)
+		if err != nil {
+			t.Fatalf("failed to hash random string: %s", err)
+		}
+		// Compared hash with original string.
+		match, err := hasher.CompareStringToHash(randomString, hashedString)
+		if err != nil {
+			t.Fatalf("original and hashed strings don't match")
+		}
+		if !match {
+			t.Fatalf("original and hashed strings don't match")
+		}
+		// Now we test the comparison when the original string and the hash don't match.
+		hasher = hashing.NewHasher(opts...)
+		randomString, err = hasher.RandomString(seed)
+		if err != nil {
+			t.Fatalf("failed to generate random string: %s", err)
+		}
+		match, err = hasher.CompareStringToHash(randomString, hashedString)
+		if err != nil {
+			t.Fatalf("failed to compare hash and original: %s", err)
+		}
+		if match {
+			t.Fatalf("original and hashed shouldn't match, comparison failed")
+		}
 	}
 }
